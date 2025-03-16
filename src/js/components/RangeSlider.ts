35bb1638064ff9bfbb4036d0d1026d0fcb352d28
lianet <jack.tolmak@yandex.ru>
2025-03-16 16:22:34 +0300

    add rangeSlider fix

--- src/js/components/RangeSlider.ts
+++ src/js/components/RangeSlider.ts
@@ -36,17 +36,26 @@ export class RangeSlider {
 
 		this.minInput.addEventListener("input", this.onInputValidation);
 		this.maxInput.addEventListener("input", this.onInputValidation);
+
+		this.addCurrencyUnit(this.minInput);
+		this.addCurrencyUnit(this.maxInput);
 	}
 
 	private onSliderUpdate = (values: (string | number)[]) => {
 		this.minInput.value = String(values[0]);
 		this.maxInput.value = String(values[1]);
+
+		this.updateUnitPosition(this.minInput);
+		this.updateUnitPosition(this.maxInput);
 	};
 
 	private onInputChange = () => {
 		const minValue = parseInt(this.minInput.value.replace(/\s/g, "")) || 0;
 		const maxValue = parseInt(this.maxInput.value.replace(/\s/g, "")) || 100;
 		this.slider.set([minValue, maxValue]);
+
+		this.updateUnitPosition(this.minInput);
+		this.updateUnitPosition(this.maxInput);
 	};
 
 	private onInputValidation = (event: Event) => {
@@ -59,5 +68,48 @@ export class RangeSlider {
 		}
 		let numericValue = parseInt(value, 10);
 		input.value = addThousandSeparator(numericValue);
+
+		this.updateUnitPosition(input);
 	};
+
+	private addCurrencyUnit(input: HTMLInputElement) {
+		const unitData = input.getAttribute("data-unit");
+
+		if (unitData) {
+			const unit = document.createElement("span");
+			unit.classList.add("form__control-unit");
+			unit.textContent = unitData;
+			input.parentNode?.appendChild(unit);
+
+			setTimeout(() => {
+				this.updateUnitPosition(input);
+			}, 0);
+		}
+	}
+
+	private getTextWidth(text: string, input: HTMLInputElement): number {
+		const span = document.createElement("span");
+		span.style.visibility = "hidden";
+		span.style.whiteSpace = "nowrap";
+		span.style.font = window.getComputedStyle(input).font;
+		span.textContent = text;
+		document.body.appendChild(span);
+		const textWidth = span.offsetWidth;
+		document.body.removeChild(span);
+
+		return textWidth;
+	}
+
+	private updateUnitPosition(input: HTMLInputElement) {
+		const textWidth = this.getTextWidth(input.value, input);
+
+		const paddingLeft = parseFloat(window.getComputedStyle(input).paddingLeft);
+
+		const unit = input.parentNode?.querySelector(".form__control-unit") as HTMLElement;
+
+		if (unit) {
+			const remUnit = 16;
+			unit.style.left = `${(textWidth + paddingLeft + 2) / remUnit}rem`;
+		}
+	}
 }
