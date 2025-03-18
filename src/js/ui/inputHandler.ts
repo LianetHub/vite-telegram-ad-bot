7464e0f8b66bdddd9d4253507b0167d97afcd75e
lianet <jack.tolmak@yandex.ru>
2025-03-18 14:36:28 +0300

    add search fix v-3

--- src/js/ui/inputHandler.ts
+++ src/js/ui/inputHandler.ts
@@ -1,8 +1,15 @@
 import { EventEmitter } from "../store/EventEmitter";
 import { toggleResetSearchBtn } from "../utils/uiActions";
+import debounce from "lodash.debounce";
 
 export class InputHandler {
+	private debouncedEmit: (event: Event) => void;
+
 	constructor(private eventEmitter: EventEmitter) {
+		this.debouncedEmit = debounce((event: Event) => {
+			this.eventEmitter.emit("filters:change", event);
+		}, 300);
+
 		document.addEventListener("input", this.handleInput.bind(this));
 	}
 
@@ -11,7 +18,7 @@ export class InputHandler {
 
 		if (target.name === "search") {
 			toggleResetSearchBtn(target.value);
-			this.eventEmitter.emit("filters:change", event);
+			this.debouncedEmit(event);
 		}
 	}
 }
